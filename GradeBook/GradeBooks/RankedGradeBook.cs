2165278085130265d92f70ac4cb6442e959a1bbb
willedstrom <willedstrom@gmail.com>
2022-02-18 13:04:38 -0800

    If this runs the first time i deserve a raise (but not ONLY if :P)

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -10,5 +11,30 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            double percentile = Students.Count / 4;
+            List<double> grades = new List<double>();
+            Students.ForEach(delegate(Student student){ grades.Add(student.Grades.Average()); });
+            grades.Sort();
+
+            List<char> gradeLetters = new List<char>() { 'A', 'B', 'C', 'D', 'F' };
+            for(int i = 0; i < grades.Count; i++)
+            {
+                if(i == grades.Count - 1 || averageGrade > grades[(int)Math.Round(percentile * (i+1))])
+                {
+                    return gradeLetters[i];
+                }
+            }
+            return 'F';
+        }
+
     }
 }
