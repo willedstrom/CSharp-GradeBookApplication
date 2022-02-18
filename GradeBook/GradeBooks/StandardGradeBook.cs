8cbe2451e7b44d23fea0a1d9be7f8ef6cbe6dc18
willedstrom <willedstrom@gmail.com>
2022-02-18 12:33:15 -0800

    Didn't know private classes were a thing I guess that's neat

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    internal class StandardGradeBook : BaseGradeBook
+    public class StandardGradeBook : BaseGradeBook
     {
         public StandardGradeBook(string name) : base(name)
         {
